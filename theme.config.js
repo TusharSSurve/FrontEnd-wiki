530b8f24875f1fe4547f9ec5205cb7c37a759125
TusharSSurve <ttusharsurve567@gmail.com>
2024-10-08 22:59:19 +0530

    [master] : Theme updated

--- theme.config.js
+++ theme.config.js
@@ -1,7 +1,7 @@
 const config = {
   logo: <span>Frontend.wiki</span>,
   project: {
-    link: 'https://github.com/TusharSSurve',
+    link: 'https://github.com/TusharSSurve/Frontend-wiki',
   },
   darkMode: true,
   footer: {
@@ -17,6 +17,57 @@ const config = {
   sidebar: {
     toggleButton: false
   },
+  head: function Head(props) {
+    return (
+      <>
+        <meta charset="utf-8" />
+        <meta name="theme-color" content="#000" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icon.svg" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icon.svg"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icon.svg"
+        />
+        <link rel="mask-icon" href="/icon.svg" color="#000000" />
+        <link rel="shortcut icon" href="/icon.svg" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta
+          name="description"
+          content="Discover a comprehensive collection of frontend development interview questions with detailed answers covering HTML, CSS, JavaScript, and React. Perfect for developers preparing for technical interviews or sharpening their web development skills."
+        />
+        <meta name="author" content="Tushar Surve" />
+        <meta
+          property="og:url"
+          content=""
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="692" />
+        <meta
+          property="og:title"
+          content='Frontend.wiki'
+        />
+        <meta
+          property="og:description"
+          content="Discover a comprehensive collection of frontend development interview questions with detailed answers covering HTML, CSS, JavaScript, and React. Perfect for developers preparing for technical interviews or sharpening their web development skills."
+        />
+        <meta
+          property="og:image"
+          content=""
+        />
+      </>
+    );
+  },
+  navigation: false
 }
 
 export default config
