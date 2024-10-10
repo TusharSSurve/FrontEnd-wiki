fe9360c8ae83d370730b7434c0cf2d0f287e8ec7
TusharSSurve <ttusharsurve567@gmail.com>
2024-10-10 23:51:10 +0530

    [master] : Basic javascript questions added

--- pages/javascript/_meta.js
+++ pages/javascript/_meta.js
@@ -1,9 +1,11 @@
 export default {
   "basic": "Basic",
+  "variable-declaration": "Variable Declaration",
   "callback": "Callback",
   "promise": "Promise",
   "closures": "Closures",
   "event-loop": "Event Loop",
   "web-storage": "Web Storage",
+  "browser": "Browser",
   "code-snippets": "Code Snippets",
 }
